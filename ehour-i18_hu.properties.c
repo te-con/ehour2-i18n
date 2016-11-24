6d1ae9f7b4f1aaa5c2f85d7a8b7159e40bfbe995
Laszlo Sandor <sandorl.headsup@gmail.com>
2016-11-25 00:06:54 +0100

    hu language file
    please upload the hu language file to a test environment so that I can start testing - I did something wrong previously as I believed I committed this already earlier

---
+++ ehour-i18_hu.properties.c
@@ -0,0 +1,324 @@
+# Top navigation
+topnav.profile=Profil
+topnav.signout=Kilépés
+nav.main.manage=Beállít
+nav.main.admin=Admin
+nav.main.hours=Időnapló
+nav.main.overview=Átnézet
+nav.main.expenses=Költségek
+nav.main.report=Riportok
+nav.main.approve=Időnapló jóváhagyása
+nav.main.settings=Általános beállítások
+nav.main.settings.ehour=eHour beállítások
+nav.main.settings.expenses=Ktg. beállításai
+nav.main.settings.locks=Lezárt napok
+nav.main.settings.billing=Számlázás
+nav.main.settings.approval=Jóváhagyás beállítása
+nav.main.settings.customfields=Spec. mezők beállításai
+nav.main.settings.api=API
+
+# Changelog page
+changelog.title=Változásnapló
+
+# Track time
+tracktime.title=Naplózd az órákat
+tracktime.search.placeholder=Vevő vagy projekt keresése
+tracktime.settings=beállítások
+tracktime.smartsorting.title=Sorrendbe állítás
+tracktime.smartsorting.on=BE
+tracktime.smartsorting.off=KI
+tracktime.smartsorting.help=Gyakori projektek előre
+tracktime.inputintime.title=Naplózás időben
+tracktime.inputintime.on=BE
+tracktime.inputintime.off=KI
+tracktime.inputintime.help=Órák írása HH:MM formátumban (1,5 helyett 1:30)
+
+tracktime.showcodes.title=Projekt kódot mutat
+tracktime.showCodes.on=BE
+tracktime.showCodes.off=KI
+tracktime.showcodes.help=Projekt kódot mutat a naplón
+
+tracktime.comment.label=Komment
+tracktime.comment.save=Mentés
+tracktime.desktop.project=Projekt
+tracktime.desktop.total=Össz.ktg
+tracktime.stored={0} óra naplózva {1} {2}-on 
+tracktime.storedcomment=Komment frissítve {0} {1}-on
+tracktime.overbudget=Budget túllépés, {0} órát próbál terhelni egy {1} órás budgetre
+
+tracktime.nothingfound=Nem találtam a szűrésnek megfelelő projektet. Csak az ezen a héten naplózható projektek láthatóak.
+
+tracktime.upate20161101.header=Az időnaplód most frissült!
+
+
+dayInput.IConverter.TrackedTime=A bevitt adat nem felismerhető (idő vagy óra)
+
+# Month overview for user
+useroverview.pagetitle=Havi összesítő
+useroverview.title={0}-án naplózott óra
+useroverview.week={0} hét
+useroverview.client=Ügyfél
+useroverview.project=Projekt
+useroverview.hours=Óra
+useroverview.turnover=Bevétel
+
+
+# Approval request and it's statuses
+approval.user.approval.pending.button=Jóváhagyásra vár
+approval.user.approval.pending.header={0} várakozik
+approval.user.approval.pending.body={0} jóváhagyása függőben.
+approval.user.approval.pending.cancel=Jóváhagyási kérelem törlése
+approval.user.approval.approved.button=Jóváhagyva
+approval.user.approval.approved.header={0} jóváhagyva
+approval.user.approval.approved.body={0} jóváhagyta {1}-án/én.
+approval.user.approval.rejected.button=Órák elutasítva
+approval.user.approval.rejected.header={0} elutasítva
+approval.user.approval.rejected.body={0} elutasította {1}-án/én 
+approval.user.approval.rejected.link=Órák újraküldése
+approval.user.approval.rejected.resubmit=Módosítás után újraküldhető jóváhagyásra.
+approval.user.approval.rejected.note=Küldj megjegyzést
+approval.user.approval.request.button=Jóváhagyás kérése
+approval.user.approval.request.header=Jóváhagyás kérése {0}-hez
+approval.user.approval.request.submit=Jóváhagyás kérése {0}-hez
+approval.user.request.body.1=Jóváhagyás kérése az összes naplózott időre
+approval.user.request.body.2=Ez lezárja az időnaplót (nem módosítható).
+approval.user.request.note=megjegyzés
+approval.user.requested=Jóváhagyás kérése
+approval.user.alreadyrequested=Jóváhagyás kérése már megtörtént
+approval.user.canceled=Kérés visszavonása
+approval.user.canceled.failed=Már nem törölhető jóváhagyás
+
+# Reporting
+report.title=Riportok
+report.customize.label=testreszabás
+report.datepicker.label=Időintervallum
+report.predefined.label=Mentett/standard riportok
+report.columns.label=Riport oszlopai
+report.grouping.label=Összesítés ezekre az oszlopokra
+report.clients.label=Vevők feltüntetése
+report.clients.all=Összes vevő
+report.projects.label=Projektek feltüntetése
+report.projects.all=Összes Projekt
+report.teams.label=Csapatok feltüntetése
+report.teams.all=Összes csapat
+report.users.label=Felhasználók feltüntetése
+report.users.all=Összes felhasználó
+report.settings.label=Beállítások
+report.settings.showcode.label=Kódok feltüntetése
+report.settings.showcode.help=Vevő, projekt és csapat kódok feltüntetése a szűrőben.
+report.export.label=Export
+report.export.excel.label=Export Excelbe
+report.storedreports=Tárolt riportok
+report.predefined.hours=Órák
+report.predefined.hours_month_client_project_user=Összes óra hónap, vevő, projekt és felhasználó szerint
+report.predefined.hours_month_client_project_task_user=Összes óra hónap, vevő, projekt, feladat és felhasználó szerint
+report.predefined.hours_month_client_user=Összes óra hónap, vevő és felhasználó szerint
+report.predefined.hours_month_project_user=Összes óra hónap, projekt és felhasználó szerint
+report.predefined.hours_month_user=Összes óra hónap és felhasználó szerint
+report.predefined.hours_all=Órák tételesen
+report.predefined.hours_approved_all=Jóváhagyás státusza
+report.predefined.your_hours_month_client_project=Egyéni órák hónap, vevő és projekt szerint
+report.predefined.your_hours_month_client_project_task=Egyéni órák hónap, vevő, projekt és feladat szerint
+report.predefined.your_hours_month_client=Egyéni órák hónap és vevő szerint
+report.predefined.your_hours_month_project=Egyéni órák hónap és projekt szerint
+report.predefined.your_hours_all=Összes egyéni óra
+report.predefined.your_hours_approved=Jóváhagyás státusza
+report.predefined.legacy=Korábbi (eHour 1.0) riportok
+report.predefined.legacy.customer=Vevő riport
+report.predefined.legacy.project=Projekt riport
+report.predefined.legacy.user=Felh. report
+report.predefined.legacy.detailed=Részletes report
+report.predefined.expenses=Költségek
+report.predefined.expenses_all_client_project_user=Összköltség hónap, vevő, projekt és felhasználó szerint
+report.predefined.expenses_all_client_user=Összköltség hónap, vevő és felhasználó szerint
+report.predefined.expenses_all_project_user=Összköltség hónap, projekt és felhasználó szerint
+report.predefined.expenses_all_user=Összköltség hónap és felhasználó szerint
+report.predefined.expenses_all=Összköltség tételesen
+report.predefined.expenses_your_client_project=Egyéni költségek hónap, vevő és projekt szerint
+report.predefined.expenses_your_client=Egyéni költségek hónap és vevő szerint
+report.predefined.expenses_your_project=Egyéni költségek hónap és projekt szerint
+report.predefined.expenses_your_month=Egyéni költségek hónap szerint
+report.predefined.expenses_your_all=Egyéni költségek összesen
+report.predefined.combined=Összevont
+report.predefined.combined_all=Órák és költségek
+report.predefined.combined_all_grouped=Órák és költségek hónap, vevő, projekt és felhasználó szerint
+report.predefined.combined_all_your=Egyéni időnapló és költségelszámolás
+report.predefined.combined_all_your_grouped=Egyéni időnapló és költségek hónap, vevő és projekt szerint
+report.predefined.customize=Testreszabás
+report.filter.label=Szűrő
+report.js.approvedHours=Jóváhagyott órák
+report.js.unapprovedHours=Nem jóváhagyott órák
+report.js.hours=Órák
+report.js.andExpenses= és költségek csoportosítva
+report.js.grouped= csoportosítva
+report.js.expensesGrouped=Költségek csoportosítva
+report.js.andGrouped= és csoportosítva
+report.js.and= és
+report.js.ungrouped=Összes
+report.js.time=Idő
+report.js.expenses=Költségek
+report.column.client=Vevő
+report.column.client-code=Vevőkód
+report.column.client-name=Vevő neve
+report.column.project=Projekt
+report.column.project-type=Típus
+report.column.project-code=Projekt kód
+report.column.project-name=Projekt név
+report.column.project-billable=Számlázható
+report.column.task=Feladat
+report.column.task-name=Feladat
+report.column.team=Csapat
+report.column.team-code=Csapat kód
+report.column.team-name=Csapat név
+report.column.user=Felh
+report.column.user-user=Felh
+report.column.user-linemanager=Vezető
+report.column.user-contracthours=Szerződött órák
+report.column.hours=Órák
+report.column.hours-hours=Órák
+report.column.hours-turnover=Bevétel
+report.column.hours-comment=Megjegyzés
+report.column.hours-tag=Címke
+report.column.hours-approved=Jóváhagyás státusza
+report.column.assignment=Kiosztás
+report.column.assignment-role=Szerep
+report.column.assignment-rate=Ár
+report.column.time=Idő
+report.column.daily=Nap
+report.column.weekly=Hét
+report.column.monthly=Hónap
+report.column.quarterly=Negyedév
+report.column.yearly=Év
+report.column.expense-cost=Költség
+report.column.expense-id=Költség kód
+report.column.expense-vat=Költség ÁFA
+report.column.expense-name=Ktg. megnevezés
+report.column.expense-comment=Ktg. megjegyzés
+report.column.expense-tag=Költség státusz
+report.column.expense-receipt=Számla
+report.column.expense-category=Ktg. kategória
+report.column.expense-category-code=Ktg.kat.kód
+report.column.expense-category-name=Ktg.kat.név
+report.column.expense-category-billable=Ktg.kat.számlázható
+report.js.moreOptions=Több opció
+report.js.lessOptions=Kevesebb opció
+report.filter.expenses=Költségek szűrése
+report.filter.expenses.all=Minden ktg. feltüntetése
+report.filter.expenses.processed=Csak feldolg. ktg-ek feltüntetése
+report.filter.expenses.unprocessed=Csak nem feldolg. ktg-ek feltüntetése
+report.filter.approved=Jóváhagyás szűrése
+report.filter.approved.all=Minden óra feltüntetése
+report.filter.approved.only_approved=Csak jóváhagyott órák
+report.filter.approved.only_unapproved=Csak nem jóváhagyott órák
+report.filter.tags=Szűrés cimkére
+report.filter.tags.all=Minden cimke
+report.filter.tags.none=Órák cimke nélkül
+report.expenses.mark=Ktg-ek jelölése
+report.expenses.mark.body.1=Minden {0} ktg. feldolgozottnak jelölve.
+report.expenses.mark.body.2=Feldolgozott ktg-ek nem módosíthatóak.
+report.expenses.unmark.body.multi={0} költségek ebben a riportban feldolgozottnak jelölve.
+report.expenses.unmark.body.single=1 költség ebben a riportban feldolgozottnak jelölve.
+report.expenses.mark.submit=feldolgozottnak jelölve
+report.expenses.undo.submit=függőre állítva
+report.taghours.remove.body.multi={0} időnapló bejegyzés jelölve.
+report.taghours.remove.body.single=1 időnapló bejegyzés jelölve.
+report.store.button=Mentés
+report.store.title=Riport mentése
+report.column.approval.enum.not-requested=Nem továbbítva
+report.column.approval.enum.pending=Függő
+report.column.approval.enum.rejected=Visszautasítva
+report.column.approval.enum.reopened=Újranyitva
+report.column.approval.enum.approved=Jóváhagyva
+
+# Claim expenses
+claimexpense.title=Ktg.elszámolás benyújtva
+claimexpense.header=Költségek {0}-ben
+claimexpense.created=Új ktg. létrehozva {0} részére {1}-án
+claimexpense.updated=Ktg. módosítva {0} részére {1}-án
+claimexpense.deleted=Költség törölve
+claimexpense.unknown=Nincs meg a költség, törölve lett?
+claimexpense.new=Új költség
+claimexpense.form.title.new=Új költség
+claimexpense.form.title.existing=Szerkeszt {0}
+claimexpense.form.name=Leírás
+claimexpense.form.choosecategory=Kategória választás
+claimexpense.form.chooseproject=Projekt választás
+claimexpense.form.category=Kategória
+claimexpense.form.client=Vevő
+claimexpense.form.date=Dátum
+claimexpense.form.project=Projekt
+claimexpense.form.cost=Ktg. (ÁFÁval)
+claimexpense.form.vat=ÁFA összeg
+claimexpense.form.comment=Megjegyzés
+claimexpense.form.receipt.upload=Számlakép feltöltés
+claimexpense.locked=Ezt a költséget a vezető lezárta, ezért nem módosítható.
+claimexpense.locked.status=LEZÁRT
+claimexpense.processed=Ezt a költséget már feldolgozták, ezért nem módosítható.
+claimexpense.processed.status=FELDOLGOZVA
+claimexpense.attachreceipts=Mellékelt számlák
+claimexpense.vat=ÁFA
+claimexpense.noexpense=Nem adtál be elszámolást
+claimexpense.newexpense=Új költség felvitele.
+claimexpense.total=Összesen
+claimexpense.save=Ktg. mentése
+claimexpense.selectreceipts=Számlakép feltöltése
+
+# Misc
+until=addig
+toggle.on=BE
+toggle.off=KI
+confirmlink.header.delete=Törlés {0}?
+confirmlink.body1.delete={0} törölve lesz.
+confirmlink.body2.delete=Biztosan törölni akarod a {0}?
+confirmlink.header.archive=Archiválni {0}?
+confirmlink.body1.archive={0} archiválva lesz.
+confirmlink.body2.archive=Biztosan archiválni akarod a {0}?
+
+
+# Profile page
+user.profile.title=Profil
+user.profile.header=Profil {0}
+user.profile.global.header=Ált. beállítások
+user.profile.smartsorting.title=Időnapló spec. rendezés
+user.profile.smartsorting.body=Időnapló spec. rendezésének bekapcsolása, ami a gyakori projekteket előre sorolja.
+user.profile.smartsorting.enabled=Spec. rendezés bekapcsolva
+user.profile.smartsorting.disabled=Spec. rendezés kikapcsolva
+user.profile.intime.title=Idő formázása
+user.profile.intime.body=HH:MM formátum bekapcsolása. Például egy óra 15 perc 01:15 formában\HH:MM formátum kikapcsolása, az idő egy óra részeként kezelve. Például egy óra 15 perc 01.25 formában.
+user.profile.intime.enabled=Az idő HH:MM formátumban (pl. 01:30)
+user.profile.intime.disabled=Az idő decimális formátumban (1.5 óra)
+user.profile.showCodes.title=Vevő, projekt, felhasználó és csapat kódok feltüntetése
+user.profile.showCodes.body=Referencia kódok feltüntetése az időnaplókon és riportokban.
+user.profile.showCodes.enabled=Referencia kódok feltüntetve.
+user.profile.showCodes.disabled=Referencia kódok rejtve.
+user.profile.password=Jelszó megváltoztatása
+user.profile.password.help=A jelszó megváltoztatásához először vidd be az aktuális jelszót, utána az újat kétszer, hogy kizárjuk az elütést.
+user.profile.password.current=Aktuális jelszó
+user.profile.password.new=Új jelszó
+user.profile.password.submit=Jelszó megváltoztatása
+user.profile.password.changed=A jelszó megváltozott
+user.profile.password.failed=Nem sikerült megváltoztatni a jelszót, jól lett begépelve az aktuális jelszó?
+
+# Manage custom fields page
+admin.customfields.title=Felh. mezők szerkesztése
+admin.customfields.client.title=Felh. mezők vevőhöz kapcsolása
+admin.customfields.project.title=Felh. mezők projekthez kapcsolása
+admin.customfields.team.title=Felh. mezők csapathoz kapcsolása
+admin.customfields.user.title=Felh. mezők felhasználóhoz kapcsolása
+admin.customfields.expense.title=Felh. mezők költséghez kapcsolása
+admin.customfields.field1=Felh. mező 1
+admin.customfields.field2=Felh. mező 2
+admin.customfields.field3=Felh. mező 3
+admin.customfields.sublabel=Nincs szükség felh. mezőre? Hagyd üresen.
+
+# API key
+admin.apikey.title=API Kulcs
+confirmlink.header.apikey=API kulcs létrehozása?
+confirmlink.body1.apikey=Új API kulcs létrehozása, az előző felülírása.
+confirmlink.body2.apikey=Biztos, hogy új kulcsot generálsz?
+
+# Task management
+admin.manage.task.add.title=Új feladat
+admin.manage.task.edit.title={0} feladat szerkesztése
+admin.manage.task.task.name=Elnevezés
+
